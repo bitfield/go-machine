21d574ecb9f90b83228125c2c49c565c33e1f959
John Arundel <john@bitfieldconsulting.com>
2026-08-31 16:48:20 +0100

    ex 2

--- r8_test.go
+++ r8_test.go
@@ -18,12 +18,17 @@ func TestNewCPU_InitialisesCPU(t *testing.T) {
 	}
 }
 
-func TestStepIncrementsPC(t *testing.T) {
+func TestStepTwiceIncrementsPCTwice(t *testing.T) {
 	t.Parallel()
 	cpu := r8.NewCPU()
 	cpu.Mem[0] = 1
+	cpu.Mem[1] = 1
 	cpu.Step()
 	if cpu.PC != 1 {
 		t.Errorf("want pc == 1, got %d", cpu.PC)
+	}
+	cpu.Step()
+	if cpu.PC != 2 {
+		t.Errorf("want pc == 2, got %d", cpu.PC)
 	}
 }
